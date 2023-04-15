610312a3a0be46405145d5703b1b9bc163ae32cc
aleksnieto <malejandronb01@gmail.com>
2023-04-15 02:36:01 +0200

    Se agrega el Proyecto

---
+++ index/index.js
@@ -0,0 +1,15 @@
+// ABOUT ME
+
+ScrollReveal().reveal('.info', {delay: 300});
+//ScrollReveal().reveal('.yo', {delay: 250});
+// ScrollReveal().reveal('.tec-text', {delay: 250});
+
+// SKILLSET
+ScrollReveal().reveal('.html5, .css3, .javascript, .nodejs, .oracle, .mysql, .java, .csharp', {delay: 650});
+
+// CARDS
+ScrollReveal().reveal('.card-1', {delay: 300});
+ScrollReveal().reveal('.card-2', {delay: 450});
+ScrollReveal().reveal('.card-3', {delay: 650});
+
+// FORM
